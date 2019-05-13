91be68bdbcbe50894a840fb3d011a7a28448ec9d
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2019-05-12 20:49:47 -0500

    fixed header syntax. added a setTimeout based debounce

--- src/App.js
+++ src/App.js
@@ -35,13 +35,24 @@ class TwitterTextArea extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.allowXHR = this.allowXHR.bind(this);
   }
-  handleChange(e) {
+  allowXHR(){
+    this.setState({ allowXHR: true});
+    console.log(3, "setTimeout is done, allowXHR is now TRUE");
+  }
+  3
     var currentText = e.target.value; // this is the entire value of textarea
     this.setState({
+      allowXHR: false,
+
       currentInput: currentText,
       charactersLeft: 280 - currentText.length
     });
+    console.log(1, "setState allowXHR FALSE");
+    console.log(2, "setTimeout to allowXHR TRUE in .5 seconds...");
+    setTimeout(this.allowXHR, 300);
+
     var currentTextArray = currentText.split(" ");
     // going to split(" ") by each word
     //now looking for any possible tagging and then making possible changes
@@ -52,47 +63,52 @@ class TwitterTextArea extends Component {
 
 
 
+        if(this.state.allowXHR){
+          this.setState({loading: true});
+          // if first character is @ and second character is letter or number
+          //continue search
+          let cleanSearch = currentTextArray[i].slice(1); // just getting rid of the @ sign for search
+          this.setState({ currentSearch: cleanSearch })
 
+          // CHECKING IF WE ALREADY HAVE SEARCHED THIS
+          if ( !Boolean(this.state.prevSearches[cleanSearch]) ) {
+              // let's search for it now
+              // then get the results and store it in THIS.STATE
+              // then we're going to put that shit on the page.
+              console.log("1 this has never been searched before");
+              console.log("2 new search!", cleanSearch);
+              var results = [];
+              axios.get("http://localhost:4000/twitter/user/search?username=" + cleanSearch)
+              .then(res => {
+                // console.log(res.data.users);
+                const allUsers = res.data.users;
+                const sixSuggestions = allUsers.slice(0, 6);
+                // console.log(sixSuggestions);
+                // looping through the 6 item array to get the info we want
+                for (let k = 0; k < sixSuggestions.length; k++) {
+                  let screen_name = sixSuggestions[k].screen_name;  // CUBS
+                  let name = sixSuggestions[k].name;  // Chicago Cubs
+                  let verified = sixSuggestions[k].verified;  // Chicago Cubs
+                  let profile_image_url = sixSuggestions[k].profile_image_url; //cubs.jpg
+                  results.push({"screen_name": screen_name, "name": name, "verified": verified, "profile_image_url": profile_image_url});
+                }
+                var thisstateprevSearches = this.state.prevSearches; //creating pseuedo object
+                thisstateprevSearches[cleanSearch] = results;  // adding to that pseudo object
+                this.setState({ prevSearches : thisstateprevSearches, loading: false});  // setting state to pseudo object
 
-
-        this.setState({loading: true});
-        // if first character is @ and second character is letter or number
-        //continue search
-        let cleanSearch = currentTextArray[i].slice(1); // just getting rid of the @ sign for search
-        this.setState({ currentSearch: cleanSearch })
-
-        // CHECKING IF WE ALREADY HAVE SEARCHED THIS
-        if ( !Boolean(this.state.prevSearches[cleanSearch]) ) {
-            // let's search for it now
-            // then get the results and store it in THIS.STATE
-            // then we're going to put that shit on the page.
-            console.log("1 this has never been searched before");
-            console.log("2 new search!", cleanSearch);
-            var results = [];
-            axios.get("http://localhost:4000/twitter/user/search?username=" + cleanSearch)
-            .then(res => {
-              // console.log(res.data.users);
-              const allUsers = res.data.users;
-              const sixSuggestions = allUsers.slice(0, 6);
-              // console.log(sixSuggestions);
-              // looping through the 6 item array to get the info we want
-              for (let k = 0; k < sixSuggestions.length; k++) {
-                let screen_name = sixSuggestions[k].screen_name;  // CUBS
-                let name = sixSuggestions[k].name;  // Chicago Cubs
-                let verified = sixSuggestions[k].verified;  // Chicago Cubs
-                let profile_image_url = sixSuggestions[k].profile_image_url; //cubs.jpg
-                results.push({"screen_name": screen_name, "name": name, "verified": verified, "profile_image_url": profile_image_url});
-              }
-              var thisstateprevSearches = this.state.prevSearches; //creating pseuedo object
-              thisstateprevSearches[cleanSearch] = results;  // adding to that pseudo object
-              this.setState({ prevSearches : thisstateprevSearches, loading: false});  // setting state to pseudo object
-
-            }) // END AXIOS CALL
+              }) // END AXIOS CALL
+          } else {
+            this.setState({loading: false});
+            console.log("a: this already has been searched");
+            console.log("b:so let's query this information");
+          }
         } else {
-          this.setState({loading: false});
-          console.log("a: this already has been searched");
-          console.log("b:so let's query this information");
+          console.warn("can't do XHR yet");
         }
+
+
+
+
 
       } else {
         // this is just a normal word being typed. NOT a tag
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class Header extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+  }
+  handleMouseOver(e){
+
+  }
   render() {
     return (
       <div id="header">
-        <h1 onHover="">
-          <i id="bird" class="fab fa-twitter"></i> Twïttėr
+        <h1 onMouseOver={this.handleMouseOver}>
+          <i id="bird" className="fab fa-twitter"></i> Twïttėr
         </h1>
       </div>
     )
