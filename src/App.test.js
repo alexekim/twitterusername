26b3aa0c73170026358e772115d2c7cd048f95e7
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2019-05-15 11:57:48 -0500

    added header tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 import Suggestion from './Suggestion';
 import Submit from './Submit';
 import Loading from './Loading';
+import Header from './Header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -15,7 +16,6 @@ it('renders child component Suggestion', ()=> {
   const div = document.createElement('div');
   ReactDOM.render(<App><Suggestion/></App>, div);
   ReactDOM.unmountComponentAtNode(div);
-
 })
 
 it('renders child component Loading', ()=> {
@@ -27,5 +27,11 @@ it('renders child component Loading', ()=> {
 it('renders child component Submit', ()=> {
   const div = document.createElement('div');
   ReactDOM.render(<App><Submit/></App>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('renders child component Header', ()=> {
+  const div = document.createElement('div');
+  ReactDOM.render(<App><Header/></App>, div);
   ReactDOM.unmountComponentAtNode(div);
 })
